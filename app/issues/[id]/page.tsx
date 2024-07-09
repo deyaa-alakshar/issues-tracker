d249beccb21f590454b266a27c197c4b0a2d77a3
deyaa-alakshar <deyaa_alakshar@outlook.com>
2024-07-09 09:45:14 +0300

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { IssueStatusBadge } from "@/app/components";
-import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import axios from "axios";
 import { notFound } from "next/navigation";
 import React from "react";
 import Markdown from "react-markdown";
-
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   const issue = await axios.get(
@@ -16,16 +17,24 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   if (typeof params.id !== "number") notFound;
 
   return (
-    <Box>
-      <Heading>{issue.data.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssueStatusBadge status={issue.data.status} />
-        <Text>{issue.data.createdAt}</Text>
-      </Flex>
-      <Card className="prose">
-        <Markdown>{issue.data.description}</Markdown>
-      </Card>
-    </Box>
+    <Grid columns={{ initial: "1", sm: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.data.title}</Heading>
+        <Flex gap="3" my="2">
+          <IssueStatusBadge status={issue.data.status} />
+          <Text>{issue.data.createdAt}</Text>
+        </Flex>
+        <Card className="prose">
+          <Markdown>{issue.data.description}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.data.id}/edit`}>Edit</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
