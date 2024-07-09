c1260bb3a764c6f410b61f11372d3ff5df723425
deyaa-alakshar <deyaa_alakshar@outlook.com>
2024-07-09 14:12:05 +0300

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -33,6 +33,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
       setIsSubmiting(false);
     } catch (error) {
       setError("An unexpected error occurrd");
