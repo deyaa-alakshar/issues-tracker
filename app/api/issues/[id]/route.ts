f554b374a39bba628793c2e03eba635e851d14bf
deyaa-alakshar <deyaa_alakshar@outlook.com>
2024-07-09 18:30:14 +0300

    Delete an issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -37,3 +37,18 @@ export async function PATCH(request: NextRequest, {params}: {params : {id: strin
 
     return NextResponse.json(updatedIssue, {status: 200})
 }
+
+export async function DELETE(request: NextRequest, {params}: {params: {id: string}}){
+    const issue = await prisma.issue.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+    
+    if(!issue)
+        return NextResponse.json({error: 'Invalid issue'}, {status: 404});
+
+    await prisma.issue.delete({
+        where: {id: parseInt(params.id)}
+    })
+    
+    return NextResponse.json({})
+}
