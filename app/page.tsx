38d99f9da988c89ca41fde96a0091815ba351799
deyaa-alakshar <deyaa_alakshar@outlook.com>
2024-07-18 11:21:36 +0300

    Lay out the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import LatestIssues from "./latestIssues";
 import { Status, User } from "@prisma/client";
 import IssueSummery from "./issueSummery";
-import { Flex } from "@radix-ui/themes";
+import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./issueChart";
 
 export interface Issue {
@@ -21,18 +21,20 @@ export default async function Home() {
     `http://localhost:3000/api/issues?latest=true`
   );
   return (
-    <Flex direction="column" gap="2">
-      <IssueSummery
-        open={data.open}
-        closed={data.closed}
-        inProgress={data.inProgress}
-      />
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummery
+          open={data.open}
+          closed={data.closed}
+          inProgress={data.inProgress}
+        />
+        <IssueChart
+          open={data.open}
+          closed={data.closed}
+          inProgress={data.inProgress}
+        />
+      </Flex>
       <LatestIssues issues={data.issues} />
-      <IssueChart
-        open={data.open}
-        closed={data.closed}
-        inProgress={data.inProgress}
-      />
-    </Flex>
+    </Grid>
   );
 }
